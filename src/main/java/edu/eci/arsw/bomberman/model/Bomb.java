9d1b73179f8bafe8e9ab3b20cc2e865aea40048a
santiago <santisocc29@gmail.com>
2023-03-27 17:56:53 -0500

    Jugador creado

--- src/main/java/edu/eci/arsw/bomberman/model/Bomb.java
+++ src/main/java/edu/eci/arsw/bomberman/model/Bomb.java
@@ -1,4 +1,16 @@
 package edu.eci.arsw.bomberman.model;
 
 public class Bomb {
+    private int x;
+    private int y;
+
+    public Bomb(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void explode() {
+        this.x = -1;
+        this.y = -1;
+    }
 }
--- src/main/java/edu/eci/arsw/bomberman/model/Player.java
+++ src/main/java/edu/eci/arsw/bomberman/model/Player.java
@@ -1,4 +1,42 @@
 package edu.eci.arsw.bomberman.model;
 
 public class Player {
+
+    private int x;
+    private int y;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setXY(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void putBomb() {
+        Bomb b1 = new Bomb(x,y);
+    }
+
+    public void moveUp() {
+        this.x = x;
+        this.y = y + 1;
+    }
+
+    public void moveDown() {
+        this.x = x;
+        this.y = y - 1;
+    }
+
+    public void moveLeft() {
+        this.x = x - 1;
+        this.y = y;
+    }
+
+    public void moveRight() {
+        this.x = x + 1;
+        this.y = y;
+    }
+
 }
