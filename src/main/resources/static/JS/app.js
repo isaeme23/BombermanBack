f046949a3fc0b3c7962c38f7998d5ec0e6421890
Edwar Lozano <edwar.lozano@mail.esucelaing.edu.co>
2023-04-07 18:49:12 -0500

    Selección de personajes avanzado

--- src/main/resources/static/JS/app.js
+++ src/main/resources/static/JS/app.js
@@ -1,3 +1,14 @@
+//Navigation
 function redirigirANombre() {
     window.location.href = "ventanaNombre.html";
+}
+
+function redirigirASeleccion() {
+    window.location.href = "seleccionModo.html";
+}
+
+//Other functions
+function guardarNombre() {
+    var valorNombre = document.getElementById("inputNombre").value;
+    localStorage.setItem("valorInput", valorNombre);
 }
