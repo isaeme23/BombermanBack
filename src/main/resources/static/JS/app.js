b4bad7a7679a011c0930b0105df9d8ca61ed0dd5
Edwar Lozano <edwar.lozano@mail.esucelaing.edu.co>
2023-04-08 22:16:32 -0500

    Ventanas previas a tablero

--- src/main/resources/static/JS/app.js
+++ src/main/resources/static/JS/app.js
@@ -1,3 +1,6 @@
+//Variables
+
+
 //Navigation
 function redirigirANombre() {
     window.location.href = "ventanaNombre.html";
@@ -7,8 +10,35 @@ function redirigirASeleccion() {
     window.location.href = "seleccionModo.html";
 }
 
+function redirigirACrear() {
+    window.location.href = "crearSala.html";
+}
+
+function redirigirAPartida() {
+    window.location.href = "partida.html";
+}
+
+function redirigirAUnirse() {
+    window.location.href = "unirseSala.html";
+}
+
 //Other functions
 function guardarNombre() {
     var valorNombre = document.getElementById("inputNombre").value;
     localStorage.setItem("valorInput", valorNombre);
 }
+
+function guardarValorB(){
+    localStorage.setItem('valorPersonaje', 'img/down/blancoDownGIF.gif');
+}
+
+function guardarImg(urlImg){
+    localStorage.setItem("imagenPersonaje", urlImg);
+}
+
+window.onload = function() {
+    var codigoGenerado = "";
+    codigoGenerado = Math.round(Math.random() * (999 - 100) + 100);
+    document.getElementById("codigo").innerHTML = codigoGenerado;
+}
+
---
+++ src/main/resources/static/JS/game.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+const grid = 64;
+const numRows = 13;
+const numCols = 21;
